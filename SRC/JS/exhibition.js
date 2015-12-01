8729ead620a2ccab014d486140e1996de8015a30
ACAN <ACXNX@users.noreply.github.com>
2015-12-01 13:07:23 +0800

    Update exhibition.js

--- SRC/JS/exhibition.js
+++ SRC/JS/exhibition.js
@@ -9,3 +9,22 @@ var exhibition = {
             "img" : "http://baiduworld.baidu.com/2015/images2015/bddt.jpg"
         }
     ],
+  //桌面百度
+    "zhuomianbaidu" : [
+        {
+            "title" : "桌面智能助手连接人与服务<br/>Smart assistant on PC，connecting people and service",
+            "content" : "桌面百度是百度官方出品的一款为数亿用户打造极速、智能、个性化的搜索信息及服务体验的产品。百度战略" +
+            "级桌面入口，提供创新轻快直达的搜索和服务创新体验；<br/>搜索再升级：智能搜索、本地搜索、语音搜索、网址直达，快捷触达信" +
+            "息；<br/>服务新体验：桌面闭环体验新玩法，搜索-服务-推送三级火箭助力，服务精准直达。",
+            "img" : "http://baiduworld.baidu.com/2015/images2015/zmbd.jpg"
+        },
+        {
+            "title" : "变色龙",
+            "content" : "百度浏览器，快速、智能、极简的精品浏览器；<br/>完美支持WEB精准定位，基于百度LBS IP & WIFI数据库，定位精度50m，助力O2O业" +
+                        "务；六重加速体系，搜索加速、海外站点加速、视频加速、淘宝加速等多重加速，为用户解决各场景卡顿问题；<br/>更有炫酷变色龙换肤" +
+                        "效果、海量插件应用和全面云安全体验,带来极致上网体验！",
+            "img" : "http://baiduworld.baidu.com/2015/images2015/bdllq.jpg"
+        }
+    ],
+    //手机助手
+    
