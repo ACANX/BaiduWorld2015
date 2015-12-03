475f2232e4d163c95b4c7bba26a4e7abe347de2c
ACAN <ACXNX@users.noreply.github.com>
2015-12-03 13:08:21 +0800

    Update hm.js

--- SRC/JS/hm.js
+++ SRC/JS/hm.js
@@ -60,3 +60,241 @@
 			}
 		}
 
+		function b() {
+			try {
+				document.documentElement.doScroll("left")
+			} catch (d) {
+				setTimeout(b, 1);
+				return
+			}
+			a()
+		}
+		var f = t,
+			d = [],
+			e;
+		document.addEventListener ? e = function() {
+			document.removeEventListener("DOMContentLoaded", e, t);
+			a()
+		} : document.attachEvent && (e = function() {
+			"complete" === document.readyState && (document.detachEvent("onreadystatechange", e), a())
+		});
+		(function() {
+			if (!f)
+				if (f = r, "complete" === document.readyState) a.A = r;
+				else if (document.addEventListener) document.addEventListener("DOMContentLoaded",
+				e, t), window.addEventListener("load", a, t);
+			else if (document.attachEvent) {
+				document.attachEvent("onreadystatechange", e);
+				window.attachEvent("onload", a);
+				var d = t;
+				try {
+					d = window.frameElement == s
+				} catch (n) {}
+				document.documentElement.doScroll && d && b()
+			}
+		})();
+		return function(b) {
+			a.A ? b() : d.push(b)
+		}
+	}()).A = t;
+	mt.event = {};
+	mt.event.c = function(a, b, f) {
+		a.attachEvent ? a.attachEvent("on" + b, function(b) {
+			f.call(a, b)
+		}) : a.addEventListener && a.addEventListener(b, f, t)
+	};
+	mt.event.preventDefault = function(a) {
+		a.preventDefault ? a.preventDefault() : a.returnValue = t
+	};
+	mt.l = {};
+	mt.l.parse = function() {
+		return (new Function('return (" + source + ")'))()
+	};
+	mt.l.stringify = function() {
+		function a(a) {
+			/["\\\x00-\x1f]/.test(a) && (a = a.replace(/["\\\x00-\x1f]/g, function(a) {
+				var b = f[a];
+				if (b) return b;
+				b = a.charCodeAt();
+				return "\\u00" + Math.floor(b / 16).toString(16) + (b % 16).toString(16)
+			}));
+			return '"' + a + '"'
+		}
+
+		function b(a) {
+			return 10 > a ? "0" + a : a
+		}
+		var f = {
+			"\b": "\\b",
+			"\t": "\\t",
+			"\n": "\\n",
+			"\f": "\\f",
+			"\r": "\\r",
+			'"': '\\"',
+			"\\": "\\\\"
+		};
+		return function(d) {
+			switch (typeof d) {
+				case "undefined":
+					return "undefined";
+				case "number":
+					return isFinite(d) ? String(d) : "null";
+				case "string":
+					return a(d);
+				case "boolean":
+					return String(d);
+				default:
+					if (d === s) return "null";
+					if (d instanceof Array) {
+						var e = ["["],
+							f = d.length,
+							n, g, k;
+						for (g = 0; g < f; g++) switch (k = d[g], typeof k) {
+							case "undefined":
+							case "function":
+							case "unknown":
+								break;
+							default:
+								n && e.push(","), e.push(mt.l.stringify(k)), n = 1
+						}
+						e.push("]");
+						return e.join("")
+					}
+					if (d instanceof Date) return '"' + d.getFullYear() + "-" + b(d.getMonth() + 1) + "-" + b(d.getDate()) + "T" + b(d.getHours()) + ":" + b(d.getMinutes()) + ":" + b(d.getSeconds()) + '"';
+					n = ["{"];
+					g = mt.l.stringify;
+					for (f in d)
+						if (Object.prototype.hasOwnProperty.call(d, f)) switch (k =
+							d[f], typeof k) {
+							case "undefined":
+							case "unknown":
+							case "function":
+								break;
+							default:
+								e && n.push(","), e = 1, n.push(g(f) + ":" + g(k))
+						}
+						n.push("}");
+					return n.join("")
+			}
+		}
+	}();
+	mt.lang = {};
+	mt.lang.e = function(a, b) {
+		return "[object " + b + "]" === {}.toString.call(a)
+	};
+	mt.lang.Ta = function(a) {
+		return mt.lang.e(a, "Number") && isFinite(a)
+	};
+	mt.lang.Va = function(a) {
+		return mt.lang.e(a, "String")
+	};
+	mt.localStorage = {};
+	mt.localStorage.C = function() {
+		if (!mt.localStorage.g) try {
+			mt.localStorage.g = document.createElement("input"), mt.localStorage.g.type = "hidden", mt.localStorage.g.style.display = "none", mt.localStorage.g.addBehavior("#default#userData"), document.getElementsByTagName("head")[0].appendChild(mt.localStorage.g)
+		} catch (a) {
+			return t
+		}
+		return r
+	};
+	mt.localStorage.set = function(a, b, f) {
+		var d = new Date;
+		d.setTime(d.getTime() + f || 31536E6);
+		try {
+			window.localStorage ? (b = d.getTime() + "|" + b, window.localStorage.setItem(a, b)) : mt.localStorage.C() && (mt.localStorage.g.expires = d.toUTCString(), mt.localStorage.g.load(document.location.hostname), mt.localStorage.g.setAttribute(a, b), mt.localStorage.g.save(document.location.hostname))
+		} catch (e) {}
+	};
+	mt.localStorage.get = function(a) {
+		if (window.localStorage) {
+			if (a = window.localStorage.getItem(a)) {
+				var b = a.indexOf("|"),
+					f = a.substring(0, b) - 0;
+				if (f && f > (new Date).getTime()) return a.substring(b + 1)
+			}
+		} else if (mt.localStorage.C()) try {
+			return mt.localStorage.g.load(document.location.hostname), mt.localStorage.g.getAttribute(a)
+		} catch (d) {}
+		return s
+	};
+	mt.localStorage.remove = function(a) {
+		if (window.localStorage) window.localStorage.removeItem(a);
+		else if (mt.localStorage.C()) try {
+			mt.localStorage.g.load(document.location.hostname), mt.localStorage.g.removeAttribute(a), mt.localStorage.g.save(document.location.hostname)
+		} catch (b) {}
+	};
+	mt.sessionStorage = {};
+	mt.sessionStorage.set = function(a, b) {
+		if (window.sessionStorage) try {
+			window.sessionStorage.setItem(a, b)
+		} catch (f) {}
+	};
+	mt.sessionStorage.get = function(a) {
+		return window.sessionStorage ? window.sessionStorage.getItem(a) : s
+	};
+	mt.sessionStorage.remove = function(a) {
+		window.sessionStorage && window.sessionStorage.removeItem(a)
+	};
+	mt.N = {};
+	mt.N.log = function(a, b) {
+		var f = new Image,
+			d = "mini_tangram_log_" + Math.floor(2147483648 * Math.random()).toString(36);
+		window[d] = f;
+		f.onload = f.onerror = f.onabort = function() {
+			f.onload = f.onerror = f.onabort = s;
+			f = window[d] = s;
+			b && b(a)
+		};
+		f.src = a
+	};
+	mt.O = {};
+	mt.O.ma = function() {
+		var a = "";
+		if (navigator.plugins && navigator.mimeTypes.length) {
+			var b = navigator.plugins["Shockwave Flash"];
+			b && b.description && (a = b.description.replace(/^.*\s+(\S+)\s+\S+$/, "$1"))
+		} else if (window.ActiveXObject) try {
+			if (b = new ActiveXObject("ShockwaveFlash.ShockwaveFlash"))(a = b.GetVariable("$version")) && (a = a.replace(/^.*\s+(\d+),(\d+).*$/, "$1.$2"))
+		} catch (f) {}
+		return a
+	};
+	mt.O.Ma = function(a, b, f, d, e) {
+		return '<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" id="' + a + '" width="' + f + '" height="' + d + '"><param name="movie" value="' + b + '" /><param name="flashvars" value="' + (e || "") + '" /><param name="allowscriptaccess" value="always" /><embed type="application/x-shockwave-flash" name="' + a + '" width="' + f + '" height="' + d + '" src="' + b + '" flashvars="' + (e || "") + '" allowscriptaccess="always" /></object>'
+	};
+	mt.url = {};
+	mt.url.k = function(a, b) {
+		var f = a.match(RegExp("(^|&|\\?|#)(" + b + ")=([^&#]*)(&|$|#)", ""));
+		return f ? f[3] : s
+	};
+	mt.url.Ra = function(a) {
+		return (a = a.match(/^(https?:)\/\//)) ? a[1] : s
+	};
+	mt.url.ha = function(a) {
+		return (a = a.match(/^(https?:\/\/)?([^\/\?#]*)/)) ? a[2].replace(/.*@/, "") : s
+	};
+	mt.url.Q = function(a) {
+		return (a = mt.url.ha(a)) ? a.replace(/:\d+$/, "") : a
+	};
+	mt.url.Qa = function(a) {
+		return (a = a.match(/^(https?:\/\/)?[^\/]*(.*)/)) ? a[2].replace(/[\?#].*/, "").replace(/^$/, "/") : s
+	};
+	h.r = {
+		Sa: "http://tongji.baidu.com/hm-web/welcome/ico",
+		W: "hm.baidu.com/hm.gif",
+		Z: "baidu.com",
+		qa: "hmmd",
+		ra: "hmpl",
+		pa: "hmkw",
+		na: "hmci",
+		sa: "hmsr",
+		oa: "hmcu",
+		p: 0,
+		m: Math.round(+new Date / 1E3),
+		protocol: "https:" === document.location.protocol ? "https:" : "http:",
+		Ua: 0,
+		Ja: 6E5,
+		Ka: 10,
+		La: 1024,
+		Ia: 1,
+		K: 2147483647,
+		X: "cc cf ci ck cl cm cp cu cw ds ep et fl ja ln lo lt nv rnd si st su v cv lv api tt u".split(" ")
+	};
