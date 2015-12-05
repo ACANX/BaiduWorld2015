0136fbc29aa949851c7b8f0611c0d8e08ee2147e
ACAN <ACXNX@users.noreply.github.com>
2015-12-05 19:00:32 +0800

    Update hwlx.js

--- SRC/JS/hwlx.js
+++ SRC/JS/hwlx.js
@@ -1,1 +1,11 @@
-
+//会务联系
+var hwlx = {
+    //主论坛
+    "zhu"  : [
+        {
+            "name" : "",
+            "tel" : "",
+            "email" : "ext_BDConference@baidu.com"
+        }
+    ],
+    //地图论坛
