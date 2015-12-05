55a9a4188590f47e2f64d744d00c9f273c5e031b
ACAN <ACXNX@users.noreply.github.com>
2015-12-05 19:01:51 +0800

    Update hwlx.js

--- SRC/JS/hwlx.js
+++ SRC/JS/hwlx.js
@@ -125,5 +125,60 @@ var hwlx = {
         */
     ],
     //人和服务论坛
+       "ren" : [
+        {
+            "name" : "刘飞",
+            "tel" : "",
+            "email" : "liufei19@baidu.com"
+        }
+        /*
+        {
+            "name" : "王海宁",
+            "tel" : "",
+            "email" : "wanghaining@baidu.com"
+        },
+        {
+            "name" : "郝娜",
+            "tel" : "",
+            "email" : "haona@baidu.com"
+        }
+        */
+    ],
+
+    //移动分发论坛
+    "yidong" : [
+        {
+            "name" : "高萌",
+            "tel" : "",
+            "email" : "gaomeng@baidu.com"
+        }
+        /*
+        {
+            "name" : "贾思南",
+            "tel" : "",
+            "email" : "jiasinan@baidu.com"
+        }
+        */
+    ],
+    //数字内容论坛
+    "shuzi" : [
+        {
+            "name" : "潘胜",
+            "tel" : "",
+            "email" : "pansheng@baidu.com"
+        }
+        /*
+        {
+            "name" : "黄振华",
+            "email" : "huangzhenhua01@baidu.com"
+        },
+        {
+            "name" : "汪婉霞",
+            "tel" : "",
+            "email" : "wengwanxia@baidu.com"
+        }
+        */
+    ],
+    //医疗论坛
     
     
