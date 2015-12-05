cca4be0f65dcf74a8d43f1f42c0fa2c03b76c7b0
ACAN <ACXNX@users.noreply.github.com>
2015-12-05 19:01:17 +0800

    Update hwlx.js

--- SRC/JS/hwlx.js
+++ SRC/JS/hwlx.js
@@ -9,3 +9,121 @@ var hwlx = {
         }
     ],
     //地图论坛
+    "map" : [
+        {
+            "name" : "吴琼",
+            //"tel" : "13910617382",
+			"tel" : "",
+            "email" : "wuqiong01@baidu.com"
+        }
+        /*
+        {
+            "name" : "谢德华",
+            "tel" : "",
+            "email" : "xiedehua@baidu.com"
+        },
+        {
+            "name" : "吴琼",
+            "tel" : "13910617382",
+            "email" : "wuqiong01@baidu.com"
+        },
+        {
+            "name" : "谢德华",
+            "tel" : "",
+            "email" : "xiedehua@baidu.com"
+        },
+        {
+            "name" : "吴琼",
+            "tel" : "13910617382",
+            "email" : "wuqiong01@baidu.com"
+        },
+        {
+            "name" : "谢德华",
+            "tel" : "",
+            "email" : "xiedehua@baidu.com"
+        },
+        {
+            "name" : "吴琼",
+            "tel" : "13910617382",
+            "email" : "wuqiong01@baidu.com"
+        },
+        {
+            "name" : "谢德华",
+            "tel" : "",
+            "email" : "xiedehua@baidu.com"
+        },
+        {
+            "name" : "吴琼",
+            "tel" : "13910617382",
+            "email" : "wuqiong01@baidu.com"
+        },
+        {
+            "name" : "谢德华",
+            "tel" : "",
+            "email" : "xiedehua@baidu.com"
+        },
+        {
+            "name" : "吴琼",
+            "tel" : "13910617382",
+            "email" : "wuqiong01@baidu.com"
+        },
+        {
+            "name" : "谢德华",
+            "tel" : "",
+            "email" : "xiedehua@baidu.com"
+        },
+        {
+            "name" : "吴琼",
+            "tel" : "13910617382",
+            "email" : "wuqiong01@baidu.com"
+        },
+        {
+            "name" : "谢德华",
+            "tel" : "",
+            "email" : "xiedehua@baidu.com"
+        },
+        {
+            "name" : "吴琼",
+            "tel" : "13910617382",
+            "email" : "wuqiong01@baidu.com"
+        },
+        {
+            "name" : "谢德华",
+            "tel" : "",
+            "email" : "xiedehua@baidu.com"
+        },
+        {
+            "name" : "吴琼",
+            "tel" : "13910617382",
+            "email" : "wuqiong01@baidu.com"
+        },
+        {
+            "name" : "谢德华",
+            "tel" : "",
+            "email" : "xiedehua@baidu.com"
+        },
+        {
+            "name" : "吴琼",
+            "tel" : "13910617382",
+            "email" : "wuqiong01@baidu.com"
+        },
+        {
+            "name" : "谢德华",
+            "tel" : "",
+            "email" : "xiedehua@baidu.com"
+        },
+        {
+            "name" : "吴琼",
+            "tel" : "13910617382",
+            "email" : "wuqiong01@baidu.com"
+        },
+        {
+            "name" : "谢德华",
+            "tel" : "",
+            "email" : "xiedehua@baidu.com"
+        }
+        */
+    ],
+    //人和服务论坛
+    
+    
