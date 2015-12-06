5c669fcc9f351b43744b11368bdd8fd88cb2fa3b
ACAN <ACXNX@users.noreply.github.com>
2015-12-06 15:07:33 +0800

    Update hyrc.js

--- SRC/JS/hyrc.js
+++ SRC/JS/hyrc.js
@@ -1,1 +1,102 @@
-
+//会议日程
+var hyrc = {
+    //主论坛
+    "zhu"  : [
+        {
+            "time" : "09:00-10:00",
+            "yicheng" : "大会签到",
+            "guests" : "",
+            "className" : "title1"
+        },
+        {
+            "time" : "10:00-10:30",
+            "yicheng" : "索引真实世界",
+            "guests" : "李彦宏<br/>百度公司创始人、董事长兼首席执行官",
+            "className" : "title2"
+        },
+        {
+            "time" : "10:30-10:50",
+            "yicheng" : "嘉宾演讲",
+            "guests" : "Travis Kalanick<br/>Uber 联合创始人兼首席执行官",
+            "className" : "title2"
+        },
+        {
+            "time" : "10:50-11:15",
+            "yicheng" : "真实世界：连接3600行",
+            "guests" : "李东旻<br/>百度移动服务群组事业部-百度地图事业部总经理",
+            "className" : "title3"
+        },
+        {
+            "time" : "11:15-11:40",
+            "yicheng" : "大数据+3600行",
+            "guests" : "王劲" +
+            "<br/>百度高级副总裁",
+            "className" : "title2"
+        },
+        {
+            "time" : "",
+            "yicheng" : "走向智能伴侣时代",
+            "guests" : "吴恩达" +
+            "<br/>百度首席科学家",
+            "className" : "title2"
+        }
+    ],
+    //地图论坛
+    "map" : [
+        {
+            "time" : "13:30-14:00",
+            "yicheng" : "签到",
+            "guests" : "",
+            "className" : "title2"
+        },
+        {
+            "time" : "14:00-14:10",
+            "yicheng" : "全新地图 连接服务",
+            "guests" : "李东旻<br/>百度移动服务群组事业部-百度地图事业部总经理",
+            "className" : "title3"
+        },
+        {
+            "time" : "14:10-14:20",
+            "yicheng" : "全新地图开启大航海时代",
+            "guests" : "邹庆<br/>地图客户端业务负责人",
+            "className" : "title2"
+        },
+        {
+            "time" : "14:25-14:40",
+            "yicheng" : "发现新大陆，品牌印记的蜕变故事",
+            "guests" : "史玉洁<br/>百度移动用户体验部（百度MUX）总监",
+            "className" : "title2"
+        },
+        {
+            "time" : "14:40-15:10",
+            "yicheng" : "地图商+ 从出行工具到服务平台",
+            "guests" : "张少宇<br/>百度地图商户平台业务部负责人<br/>董小玲<br/>万达电商副总裁",
+            "className" : "title4"
+        },
+        {
+            "time" : "15:10-15:30",
+            "yicheng" : "人车合一 ● 共建生态",
+            "guests" : "张辉<br/>百度车联网首席产品架构师",
+            "className" : "title2"
+        },
+        {
+            "time" : "15:30-16:00",
+            "yicheng" : "地图的开放与连接",
+            "guests" : "李志堂<br/>百度地图开放平台业务部负责人<br/>牛伟<br/>北京万科城市研究院院长<br/>马自强<br/>小马" +
+            "管家 CTO<br/><br/>于杨<br/>E代驾高级技术总监",
+            "className" : "title2"
+        },
+        {
+            "time" : "16:00-16:30",
+            "yicheng" : "新地图产业承载的O2O创新",
+            "guests" : "郭阳<br/>百度地图车生活业务部总经理<br/>刘涛<br/>地图出行业务部总经理<br/> <br/>屠静<br/>百度地图酒店总经理",
+            "className" : "title6"
+        },
+        {
+            "time" : "16:30<span style='opacity:0'>-16:30</span>",
+            "yicheng" : "Ending",
+            "guests" : "",
+            "className" : "title1"
+        }
+    ],
+    //连接人和服务论坛
