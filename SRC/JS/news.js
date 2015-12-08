bd8dc2e8244fbe377f8c5d077ff3d46213539244
ACAN <ACXNX@users.noreply.github.com>
2015-12-08 12:34:12 +0800

    Update news.js

--- SRC/JS/news.js
+++ SRC/JS/news.js
@@ -7,3 +7,7 @@ var news = [
         "title" : "百度世界大会：Uber 和人工智能是秀吗？",
         "src" : "http://news.91.com/it/1509/21871362.html"
     },
+ {
+        "title" : "百度王劲：百度大数据将成商业新能源",
+        "src" : "http://news.91.com/it/1509/21871916.html"
+    },
