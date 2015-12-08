b43874017dc0019fe5085b88754830b61e63ab3b
ACAN <ACXNX@users.noreply.github.com>
2015-12-08 12:33:50 +0800

    Update news.js

--- SRC/JS/news.js
+++ SRC/JS/news.js
@@ -3,3 +3,7 @@ var news = [
         "title" : "李彦宏百度世界主题报告 索引真实世界",
         "src" : "http://news.91.com/it/1509/21871770.html"
     },
+ {
+        "title" : "百度世界大会：Uber 和人工智能是秀吗？",
+        "src" : "http://news.91.com/it/1509/21871362.html"
+    },
