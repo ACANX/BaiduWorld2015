b656e4921d0a9e89e47245d5faa14b041005cc53
ACAN <ACXNX@users.noreply.github.com>
2015-12-09 11:13:49 +0800

    Update sketch.js

--- SRC/JS/sketch.js
+++ SRC/JS/sketch.js
@@ -1,1 +1,34 @@
 
+/* Copyright (C) 2013 Justin Windle, http://soulwire.co.uk */
+
+(function ( root, factory ) {
+    
+    if ( typeof exports === 'object' ) {
+
+        // CommonJS like
+        module.exports = factory(root, root.document);
+
+    } else if ( typeof define === 'function' && define.amd ) {
+
+        // AMD
+        define( function() { return factory( root, root.document ); });
+
+    } else {
+
+        // Browser global
+        root.Sketch = factory( root, root.document );
+    }
+
+}( typeof window !== "undefined" ? window : this, function ( window, document ) {
+
+
+    "use strict";
+
+    /*
+    ----------------------------------------------------------------------
+
+        Config
+
+    ----------------------------------------------------------------------
+    */
+
