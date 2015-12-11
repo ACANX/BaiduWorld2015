6627d755505bc3a2faaa0231f703bfa7198bb4f7
ACAN <ACXNX@users.noreply.github.com>
2015-12-11 12:52:39 +0800

    Update TweenMax.js

--- SRC/JS/TweenMax.js
+++ SRC/JS/TweenMax.js
@@ -7346,7 +7346,7 @@ if (_gsScope._gsDefine) { _gsScope._gsQueue.pop()(); } //necessary in case Tween
 
 		_tickerActive = false; //ensures that the first official animation forces a ticker.tick() to update the time when it is instantiated
 
-})((typeof(module) !== "undefined" && module.exports && typeof(global) !== "undefined")		
+})((typeof(module) !== "undefined" && module.exports && typeof(global) !== "undefined")	 ? global : this || window, "TweenMax");	
 
 
 
