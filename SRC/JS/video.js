9a4c96a2fdc4c12efddaa2889eb2488fe6777a6d
ACAN <ACXNX@users.noreply.github.com>
2015-12-12 16:48:09 +0800

    Update video.js

--- SRC/JS/video.js
+++ SRC/JS/video.js
@@ -1,1 +1,21 @@
+var video = {
+    //主论坛
+    "zhu"  : [
+        {
+            "title" : "百度世界2015主论坛全程回顾",
+            "embed" : '<embed src="http://player.video.qiyi.com/f5bc8cc248d6629914b5f3082bb21442/0/0/v_19rrodwrx0.swf-albumId=395916600-tvId=395916600-isPurchase=0-cnId=30" allowFullScreen="true" quality="high" width="581" height="345" align="middle" allowScriptAccess="always" type="application/x-shockwave-flash"></embed>',
+            "src" : "http://baiduworld.baidu.com/2015/images2015/sp_img01.png"
+        },
+        {
+            "title" : "百度世界2015李彦宏演讲：索引真实世界",
+            "embed" : '<embed src="http://player.video.qiyi.com/07179f044066e8e3341967de14e4a800/0/0/v_19rrodyoy4.swf-albumId=395888800-tvId=395888800-isPurchase=0-cnId=30" allowFullScreen="true" quality="high" width="581" height="345" align="middle" allowScriptAccess="always" type="application/x-shockwave-flash"></embed>',
+            "src" : "http://baiduworld.baidu.com/2015/images2015/sp_img02.png"
+        }
+    ],
+    //地图论坛
+    "map" : [
+    ],
+    //人和服务论坛
+    "ren" : [
 
+    ],
